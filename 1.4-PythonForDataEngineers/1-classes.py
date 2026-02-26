4989aa15119141da86c576ca15a25afd64ee5d77
shaankhandev808 <shaankhandev808@gmail.com>
2026-02-26 01:53:38 -0800

    Update 1-classes.py

--- 1.4-PythonForDataEngineers/1-classes.py
+++ 1.4-PythonForDataEngineers/1-classes.py
@@ -1,24 +1,27 @@
 # Here we learn about classes in Python.
 # The thing to realize is that a Class providess a blueprint for an Object.
 
-from datetime import datetime as dt, date
+# First we import some modules.
+from datetime import datetime, date
 from dateutil.relativedelta import relativedelta
 
+# Create a MyAge Class. A class definition has attributes and functions.
 class MyAge:
+    # Init is like a Constructor in Java. Takes self + two pieces of data for attributes. 
     def __init__(self, date_of_birth, my_name):
-        # __init__ function to fill properties and use self
-        # your birth date input yyyy-mm-dd
-        self.__date_of_birth = dt.strptime(date_of_birth, '%Y-%m-%d')
+        # double underscores indicate "private-ish" attributes, meant for internal use in the Class.
+        # Datetime() is used to parse a string into a datetime object.
+        self.__date_of_birth = datetime.strptime(date_of_birth, '%Y-%m-%d')     
         self.__my_name = my_name
+        # Find the delta between today's date and the given birthday.
         self.__my_age_years = relativedelta(
             date.today(), self.__date_of_birth).years
 
-    # create print function
+    # Classes can also have methods.
     def show_me_my_age(self):
         return f"{self.__my_name}, you are so young, only {self.__my_age_years} years old!"
 
-
-# instantiate the class and execute the print function
+# Instantiate the class, thus creating an object. Make a function call then print its result.
 age = MyAge("1988-08-08", "Shaan Khan aka Mac Daddy")
 print(age.show_me_my_age())
-# > 'Mr James, you are so young, only 39 years old!'
+# > 'Mr SHaan Khan aka Mac Daddy, you are so young, only 37 years old!'
